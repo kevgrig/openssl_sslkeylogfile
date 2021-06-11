5a8abbf3e597c77418d676dfecffc161b4be7078
Kevin <kevin@myplaceonline.com>
2021-06-11 12:51:18 -0700

    Override SSL_CTX_new

--- Makefile
+++ Makefile
@@ -1,12 +1,12 @@
 CC = gcc
-CFLAGS = -g
+CFLAGS = -g -Wall
 
 default: all
 
 all: keylogfile
 
 keylogfile: keylogfile.c
-	$(CC) $(CFLAGS) -o openssl_keylogfile.so -shared -fPIC keylogfile.c -D_GNU_SOURCE
+	$(CC) $(CFLAGS) -o openssl_keylogfile.so -shared -fPIC keylogfile.c -lcrypto -lssl -I$(OPENSSLDEVEL) -I$(OPENSSLDEVEL)/include
 
 clean:
 	rm -f openssl_keylogfile.so
--- keylogfile.c
+++ keylogfile.c
@@ -1,5 +1,29 @@
+#define _GNU_SOURCE
+#include <stdlib.h>
 #include <stdio.h>
+#include <dlfcn.h>
+#include <ssl/ssl_local.h>
 
-void test() {
-  printf("Hello World\n");
+static void keylog_callback(const SSL *ssl, const char *line) {
+  fprintf(stderr, "%s\n", line);
+}
+
+SSL_CTX *SSL_CTX_new(const SSL_METHOD *meth) {
+  SSL_CTX* (*real_SSL_CTX_new)(const SSL_METHOD *meth) = dlsym(RTLD_NEXT, "SSL_CTX_new");
+  if (!real_SSL_CTX_new) {
+    fprintf(stderr, "Could not find real SSL_CTX_new\n");
+    exit(1);
+  }
+  #ifdef DEBUG
+  fprintf(stderr, "Calling real SSL_CTX_new\n");
+  #endif
+  SSL_CTX *ctx = real_SSL_CTX_new(meth);
+  if (ctx != NULL) {
+    ctx->keylog_callback = keylog_callback;
+    fprintf(stderr, "[%s:%d:%s] Set custom keylog_callback to %p\n", __FILE__, __LINE__, __func__, ctx->keylog_callback);
+  }
+  #ifdef DEBUG
+  fprintf(stderr, "Return ctx %p\n", ctx);
+  #endif
+  return ctx;
 }
